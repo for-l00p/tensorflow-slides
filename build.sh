f79111200e0de7d77e0c659b98f90502266e57b8
최상훈 <shchoi@diotek.co.kr>
2015-12-17 14:12:16 +0900

    일부 오류 수정

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-markdown-to-slides tensorflow.md -o tensorflow.html -s remark-template-basic.css -t TensorFlow
+markdown-to-slides tensorflow.md -o tensorflow.html -s remark-template-basic.css -t TensorFlow $1
